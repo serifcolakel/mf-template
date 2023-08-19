d2452bd3a68d7181b28e9426e407d950ce60d4ef
serifcolakel <serifcolakel0@gmail.com>
2023-08-19 15:27:46 +0300

    ADDED (serif) : 🎉 store provider added.

---
+++ container/src/providers/StoreProvider.tsx
@@ -0,0 +1,7 @@
+import React, { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import { store } from "../store";
+
+export default function ReduxProvider({ children }: PropsWithChildren) {
+  return <Provider store={store}>{children}</Provider>;
+}
