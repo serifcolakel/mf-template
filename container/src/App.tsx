5fa9063dcab67ae1a045c2bafe249dc5e8962ea3
serifcolakel <serifcolakel0@gmail.com>
2023-08-19 15:29:42 +0300

    UPDATED (serif) : 🎉 container App updated for storeprovider.

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -2,21 +2,28 @@ import React, { Suspense } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 import "./index.css";
+import StoreProvider from "./providers/StoreProvider";
 const TestPage = React.lazy(() => import("remote/TestPage"));
 
-const App = () => (
-  <div className="max-w-6xl mx-auto mt-10 text-3xl text-red-600">
-    <div>Name: container</div>
-    <div>Framework: React</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-    <Suspense fallback={<div>Loading...</div>}>
-      <TestPage />
-    </Suspense>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="max-w-6xl mx-auto mt-10 text-3xl text-red-600">
+      <div>Name: container</div>
+      <div>Framework: React</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Tailwind</div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TestPage />
+      </Suspense>
+    </div>
+  );
+};
 
 const container = document.getElementById("app");
 const root = ReactDOMClient.createRoot(container!);
 
-root.render(<App />);
+root.render(
+  <StoreProvider>
+    <App />
+  </StoreProvider>
+);
